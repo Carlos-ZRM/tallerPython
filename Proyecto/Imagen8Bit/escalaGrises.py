6e72b4ed5e1dba6de8294d94a362d4a33e4ac88e
Carlos Zacarías Reyes Martínez <carloszrm90@gmail.com>
2020-10-08 03:29:38 +0000

    Mensaje

---
+++ Proyecto/Imagen8Bit/escalaGrises.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+def imprimirImg(data, tamanio):
+  k = 0 
+  file = open("ascii.txt","w+")
+  for i in range(tamanio[0]):
+    linea = ""
+    for j in range(tamanio[0]):
+      ascii = chr(data[k][0]+32)
+      linea = linea+ascii
+      k = k + 1 
+    file.write(linea)
+
+
+
+img = Image.open('crash.png').convert('LA')
+data = img.getdata()
+data = list(data)
+#print(data)
+imprimirImg(data, img.size)
+img.save('greyscale.png')

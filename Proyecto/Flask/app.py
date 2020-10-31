b136bff92f308a1bdf2eb7f7f5458f7abc41a8cc
Carlos Zacarías Reyes Martínez <carloszrm90@gmail.com>
2020-10-31 19:27:21 +0000

    flask app

--- Proyecto/Flask/app.py
+++ Proyecto/Flask/app.py
@@ -1,10 +1,30 @@
+# librerias
 from flask import Flask
+import random
+
+# crear una aplicacion 
 app = Flask(__name__)
 
+# decoradores # netflix.com/pelicula300 
+# se crea la ruta raiz netflix.com/
 @app.route('/')
 def hello_world():
+    # diccionario
+    o = {"mandarina":"fruta", "caña":"fruta", "mole":"comida" }
+    # tipo de dato es lista 
+    # list convirtio dictlist a list 
+    llaves = list(o.keys())
+    # llaves = ['mandarina', 'caña', 'mole']
+    # aleatorio es del tipo de dato entero 
+    aleatorio = random.randint(0, len(llaves)-1 ) 
+    # llave_ale es de tipo de dato string 
+    llave_ale = llaves[aleatorio]
+    # asi se accede a un diccionario 
+    key = llave_ale
+    value = o[key]
     
-    return 'Hola amigos de Geeky Theory!'
+    return 'Tu eres '+ value + " \n"+key
+
 
 if __name__ == '__main__':
     app.run()
